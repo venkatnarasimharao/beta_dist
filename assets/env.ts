08ffe1a01daeff45aba7db8b9a722e11e8a14545
Venkata Narasimha Rao Golagani <venkatnarasimharao94@gmail.com>
2021-02-23 23:08:31 +0530

    latest dist

--- assets/env.ts
+++ assets/env.ts
@@ -1,5 +1,6 @@
 export const serverIP: any = {
     apiUrl: 'http://52.15.209.9:3333/api',
+    // apiUrl: 'http://localhost/api',
     imgUrl: 'http://52.15.209.9:3333/',
     SECURITY_KEY : 'BaCaTaSTore#$.@JA'
 }
