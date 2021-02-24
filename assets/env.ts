eb15862faadf659e74a853f5cf87bdbacc62089e
uppu540 <upendra.giduturi540@outlook.com>
2021-02-24 08:37:54 +0530

    latest dist

--- assets/env.ts
+++ assets/env.ts
@@ -1,6 +1,6 @@
 export const serverIP: any = {
     apiUrl: 'http://52.15.209.9:3333/api',
-    // apiUrl: 'http://localhost/api',
+    // apiUrl: 'http://localhost:3333/api',
     imgUrl: 'http://52.15.209.9:3333/',
     SECURITY_KEY : 'BaCaTaSTore#$.@JA'
 }
