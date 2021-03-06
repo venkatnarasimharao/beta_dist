641423311997e0ed09880a9f42cc0711059bc516
Venkata Narasimha Rao Golagani <venkatnarasimharao94@gmail.com>
2021-03-06 15:48:12 +0530

    p

--- assets/env.ts
+++ assets/env.ts
@@ -1,6 +1,8 @@
 export const serverIP: any = {
+    PRODUCTION: true,
     apiUrl: 'http://52.15.209.9:3333/api',
     // apiUrl: 'http://localhost:3333/api',
+    // PRODUCTION: false,
     imgUrl: 'http://52.15.209.9:3333/',
     SECURITY_KEY : 'BaCaTaSTore#$.@JA'
 }
